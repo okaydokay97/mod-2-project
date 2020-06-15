cc340af794865447ce0bfd09c4cd1cc214fdbb5c
okaydokay97 <rubens.fernandes97@gmail.com>
2020-06-15 11:03:36 -0500

    create models, migrations, seeds, and associations

---
+++ db/migrate/20200615153156_create_movies.rb
@@ -0,0 +1,12 @@
+class CreateMovies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :genre
+      t.string :description
+      t.integer :runtime
+
+      t.timestamps
+    end
+  end
+end
