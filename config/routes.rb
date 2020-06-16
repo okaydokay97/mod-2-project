6bca347f25631a9a03971c2b7c830980baf20654
Yann233 <yannxyang@sina.com>
2020-06-16 10:08:52 -0500

    create account

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  resources :users
   resources :reviews
   resources :movies, only: [:show, :index]
   resources :roles, only: [:show]
   resources :actors, only: [:show, :index]
+  resources :users, only: [:show, :new, :create,:edit,:update] do
+    resources :reviews, only: [:destroy]
+  end
+
+  get "signup", to: "users#new", as: "signup"
+  get "login", to: "sessions#new", as: "login"
+  post "sessions", to: "sessions#create", as: "sessions"
+  delete "sessions", to: "sessions#destroy" #logout
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
