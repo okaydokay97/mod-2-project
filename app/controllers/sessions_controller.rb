315dd67cf9dc8571f9d86188a9a0157d93bf9173
Yann233 <yannxyang@sina.com>
2020-06-16 17:00:15 -0500

    create account+login worked

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
   def new
-    # @user = User.new
+    @user = User.new
   end
 
   def create
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.user_id
+      session[:user_id] = @user.id
       redirect_to @user
     else
       flash[:message] = "Authentication fail"
