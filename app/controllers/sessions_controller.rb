ed75026452fb064c753f59f2fc1c51cff87c51c4
Yann233 <yannxyang@sina.com>
2020-06-16 16:26:01 -0500

    Tue 4:30

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,25 @@
 class SessionsController < ApplicationController
   def new
-    @user = User.new
+    # @user = User.new
   end
 
   def create
-    @user = User.find_by(user_name: params[:user_name])
+    @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
+      session[:user_id] = @user.user_id
       redirect_to @user
     else
-      flash[:message] = "Invalid credentials"
+      flash[:message] = "Authentication fail"
       redirect_to login_path
     end
   end
 
-  def destroy
-    session.delete(:user_id)
-    # session[:user_id] = nil
-    flash[:notice] = 'Logout Successful'
-    redirect_to login_path
-  end
+
+  # def destroy
+  #   session.delete(:user_id)
+  #   # session[:user_id] = nil
+  #   flash[:notice] = 'Logout Successful'
+  #   redirect_to login_path
+  # end
 
 end
