a2f7c00878e4bc39afc8630b27d6b8b55b911a3d
okaydokay97 <rubens.fernandes97@gmail.com>
2020-06-18 13:35:30 -0500

    fixed multiple reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,6 @@ class Review < ApplicationRecord
   belongs_to :user
   validates :rating, presence: true, inclusion: {in: (0..10)}
   validates :content, presence: true
+  validates :user_id, uniqueness: {scope: :movie, message: "You can only write one review per movie!"}
   
 end
